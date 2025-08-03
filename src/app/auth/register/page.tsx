7420d1e9f47c5a587815b00d240e1c20cc84deb4
yashgoel <yash.goel8370@gmail.com>
2025-08-03 14:36:41 +0530

    Renamed page.tsx -> tooltip.tsx

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import Tooltip from "@/app/Tooltip/page";
+import Tooltip from "@/app/Tooltip/tooltip";
 import Image from "next/image";
 import logo from "@/assets/cleit.png";
 import Footer from "../Footer/page";
