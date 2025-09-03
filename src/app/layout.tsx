e04cf6da519d4c5da5d8b422f21e9afc141095b2
yashgoel <yash.goel8370@gmail.com>
2025-09-03 19:18:19 +0530

    feat: add meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Cleit Admin",
-  description: "",
+  title: "Cleit Admin - Vivekananda Institute of Professional Studies - TC",
+  description: "A unified platform for all the societies of Vivekananda Institute of Professional Studies — explore, wishlist, track events, set reminders, and never miss an opportunity to participate.",
 };
 
 export default function RootLayout({
